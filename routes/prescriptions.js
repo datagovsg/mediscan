755afdb2937d7923264dca5718528e75daf97460
iewil <liwei@data.gov.sg>
2018-01-05 01:58:30 +0800

    Add routes for prescriptions
    GET /prescriptions
    GET: /prescriptions/create 
    POST: /prescriptions
    POST: /prescriptions/:id/delete

--- routes/prescriptions.js
+++ routes/prescriptions.js
@@ -23,18 +23,81 @@ router.get('/', async (req, res) => {
     })
   );
 
-  res.send(JSON.stringify(prescriptions));
+  res.render('prescriptions/index', {prescriptions: prescriptions});
+});
+
+// GET: /prescriptions
+router.get('/prescriptions', async (req, res) => {
+  let prescriptions = await Prescription.find()
+    .select('-__v')
+    .lean();
+
+  // Map medication IDs to medication objects
+  await Promise.all(
+    _.map(prescriptions, async (prescription) => {
+      prescription.medications = await Medication.find({
+        _id: {$in: prescription.medications},
+      })
+        .select('-__v')
+        .lean();
+    })
+  );
+
+  res.render('prescriptions/index', {prescriptions: prescriptions});
+});
+
+// GET: /prescriptions/create
+router.get('/prescriptions/create', function(req, res, next) {
+  res.render('prescriptions/create', {
+    prescription: {
+      name: '',
+      phoneNumber: '',
+      medications: [
+        {
+          name: '',
+          quantity: '',
+          units: '',
+          remarks: '',
+          frequency: '',
+        }],
+      },
+  });
+});
+
+// POST: /prescriptionis/remind
+router.post('/:id/remind', async (req, res) => {
+  const id = req.params.id;
+  const prescription = await Prescription.findOne({_id: id});
+  await prescription.sendReminder();
+
+  res.send();
 });
 
 // POST: /prescriptions
 router.post('/', async (req, res) => {
-  const {name, phoneNumber} = req.body;
 
-  // Create and save prescription and medications
+  function convertArrayToMedication(input) {
+    return new Medication({
+      name: input[0],
+      quantity: input[1],
+      units: input[2],
+      frequency: input[3],
+      remarks: input[4] || '',
+    });
+  }
+
+  const {name, phoneNumber} = req.body;
+  const medications = _.zip(req.body.medicine,
+    req.body.quantity,
+    req.body.unit,
+    req.body.frequency,
+    req.body.remark).map(x => convertArrayToMedication(x));
+
   const medications = _.map(
     req.body.medications,
     (medication) => new Medication(medication)
   );
+
   const prescription = new Prescription({
     name,
     phoneNumber,
@@ -48,16 +111,25 @@ router.post('/', async (req, res) => {
   // Send the first message
   prescription.sendNotification(`Hi ${name},\n\nWelcome to Mediscan!`);
 
-  res.send();
+  res.redirect('/');
+});
+
+// POST: /prescriptions/:id/delete
+router.post('/prescriptions/:id/delete', async (req, res) => {
+  const id = req.params.id;
+  await Prescription.remove({_id: id});
+
+  // res.send();
+  res.redirect('/');
 });
 
 // DELETE: /prescriptions
 router.delete('/:id', async (req, res) => {
   const id = req.params.id;
 
-  await Prescription.findOne({id}).remove();
+  await Prescription.remove({_id: id});
 
-  res.send();
+  res.redirect('/');
 });
 
 module.exports = router;
