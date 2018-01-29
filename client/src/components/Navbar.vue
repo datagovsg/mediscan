5ea6cc04fd69e610dbaf30ba77919d0423a9878e
ratsjosh <ratsjosh@outlook.com>
2018-01-29 18:23:25 +0800

    Change navbar links

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,25 +1,22 @@
 <template>
   <nav class="navbar is-transparent">
     <div class="navbar-brand">
-      <a class="navbar-item" href="https://bulma.io">
+      <!-- <a class="navbar-item" href="https://bulma.io">
         <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
+      </a> -->
+      <a class="navbar-item" href="/">
+        <h1 class="title">Mediscan</h1>
       </a>
-      <div class="navbar-burger burger" data-target="navbarExampleTransparentExample" @click="toggleMenu">
+      <div class="navbar-burger burger" data-target="navbar" @click="toggleMenu">
         <span></span>
         <span></span>
         <span></span>
       </div>
     </div>
 
-    <div id="navbarExampleTransparentExample" class="navbar-menu" :class="{'is-active': navIsActive}">
-      <div class="navbar-start">
-        <a class="navbar-item" href="https://bulma.io/">
-          Home
-        </a>
-      </div>
-
+    <div id="navbar" class="navbar-menu" :class="{'is-active': navIsActive}">
       <div class="navbar-end">
-        <div class="navbar-item">
+        <!-- <div class="navbar-item">
           <div class="field is-grouped">
             <p class="control">
               <a class="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet" data-social-target="http://localhost:4000" target="_blank" href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=http://localhost:4000&amp;via=jgthms">
@@ -31,16 +28,8 @@
                 </span>
               </a>
             </p>
-            <p class="control">
-              <a class="button is-primary" href="https://github.com/jgthms/bulma/archive/0.5.1.zip">
-                <span class="icon">
-                  <i class="fas fa-download"></i>
-                </span>
-                <span>Download</span>
-              </a>
-            </p>
           </div>
-        </div>
+        </div> -->
       </div>
     </div>
   </nav>
